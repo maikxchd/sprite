c369713cd2e5cc590da2ed2560704c1b99be9f88
maik <36390867+maikxchd@users.noreply.github.com>
2023-06-24 18:45:22 +0700

    Update sprite.sh

--- sprite.sh
+++ sprite.sh
@@ -1,63 +1,45 @@
-sprite()
-{
-	if [ $VEC -ge 32 ]
-	then
-		MASK_FILL=""
-		MASK_ZERO="00000000"
-		let "IDX = $VEC / 32"
-		for ((i=1; 1<=$IDX;i++))
-		do
-			MASK_FILL="${MASK_FILL},${MASK_ZERO}"
-		done
+#!/bin/bash
 
-		let "VEC -= 32 * $IDX"
-		MASK_TMP=$((1<<$VEC))
-		MASK='printf "%X%s" $MASK_TMP $MASK_FILL'
+bind_queue_to_core() {
+    local dev=$1
+    local dir=$2
+    local irq_vec
 
-	else
-		MASK_TMP=$((1<<$VEC))
-		MASK='printf "%X" $MASK_TMP'
-	fi
+    max_irq=$(grep -Ei "$dev:.*$dir" /proc/interrupts | wc -l)
+    if [ "$max_irq" -eq 0 ]; then
+        echo "No $dir vectors found on $dev"
+        return
+    fi
 
-	printf "%s mask=%s for /proc/irq/%d/smp_affinity\n" $DEV $MASK $IRQ
-	printf "%s" $MASK > /proc/irq/$IRQ/smp_affinity
+    for ((vec = 0; vec < max_irq; vec++)); do
+        irq=$(awk -v dev="$dev" -v dir="$dir" -v vec="$vec" '$0 ~ dev "-" dir "-" vec"$" {gsub(":", "", $1); print $1}' /proc/interrupts)
+        if [ -n "$irq" ]; then
+            mask=$((1 << (vec % 32)))
+            mask_hex=$(printf "%X" $mask)
+            mask_fill=$(printf "%0${vec/32}d" 0)
+            full_mask="${mask_hex}${mask_fill}"
+            printf "%s mask=%s for /proc/irq/%d/smp_affinity\n" "$dev" "$full_mask" "$irq"
+            echo "$full_mask" > "/proc/irq/$irq/smp_affinity"
+        fi
+    done
 }
 
-if [ "$1" = "" ] ; then
-	echo "Description:"
-	echo "	This script attempts to bind each queue of a multi-queue NIC"
-	echo "	to the same numbered core, ie tx0|rx0 --> cpu0, tx1|rx1 --> cpu1"
-	echo "usage:"
-	echo "	$0 eth0 [eth1 eth2 eth3]"
+if [ $# -eq 0 ]; then
+    echo "Description:"
+    echo "    This script attempts to bind each queue of a multi-queue NIC"
+    echo "    to the same numbered core, i.e., tx0|rx0 --> cpu0, tx1|rx1 --> cpu1"
+    echo "Usage:"
+    echo "    $0 eth0 [eth1 eth2 eth3]"
+    exit 1
 fi
 
-IRQBALANCE_ON=`ps ax | grep -v grep | grep -q irqbalance; echo $?`
-if [ "$IRQBALANCE_ON" == "0" ] ; then
-	echo " WARNING : irqbalance is running and will"
-	echo "			 likely override this script\'s affinitization."
-	echo "			 Please stop the irqbalance service and/or execute"
-	echo "			 \'killall irqbalance\'"
+if pgrep irqbalance >/dev/null; then
+    echo "WARNING: irqbalance is running and may override this script's affinitization."
+    echo "         Please stop the irqbalance service and/or execute 'killall irqbalance'."
 fi
 
-for DEV in $*
-do
-	for DIR in rx tx TxRx
-	do
-		MAX='grep $DEV-$DIR /proc/interrupts | wc -1'
-		if [ "$MAX" == "0" ] ; then
-			MAX='egrep -i "$DEV:.*$DIR" /proc/interrupts | wc -1'
-		fi
-		if [ "$MAX" = "0" ] ; then
-			echo no $DUR vectorx found on $DEC
-			continue
-		fi
-		for VEC in `seq 0 1 $MAX`
-		do
-				IRQ=`cat /proc/interrupts | grep -i $DEV-$DIR-$VEC "$" | cut -d; -f1 | sed "s///g"`
-				if [ -n "$IRQ" ]; then
-					sprite
-				fi
-			fi
-		done
-	done
+for dev in "$@"; do
+    for dir in rx tx TxRx; do
+        bind_queue_to_core "$dev" "$dir"
+    done
 done
